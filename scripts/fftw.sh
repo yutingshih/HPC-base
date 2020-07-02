e4708fd8b87b6b7b0525781e8a422dfe72cfbd8e
tings0802 <nick19980922@gmail.com>
2020-07-02 16:20:09 +0800

    refactor testfftw() with recursive call

--- scripts/fftw.sh
+++ scripts/fftw.sh
@@ -101,15 +101,8 @@ function testfftw() {
 		echo ${ICC_BENCH_NOSIMD}
 		eval ${ICC_BENCH_NOSIMD}
 	elif [[ ${OPT} == '--all' ]]; then
-		module purge; module load icc
-		echo ${GCC_BENCH}
-		eval ${GCC_BENCH}
-		echo ${GCC_BENCH_NOSIMD}
-		eval ${GCC_BENCH_NOSIMD}
-		echo ${ICC_BENCH}
-		eval ${ICC_BENCH}
-		echo ${ICC_BENCH_NOSIMD}
-		eval ${ICC_BENCH_NOSIMD}
+		testfftw --gcc ${SIZE}
+		testfftw --icc ${SIZE}
 	else
 		echo "testfftw: ${OPT} option not found"
 		exit 1;
