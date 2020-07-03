3ac4f7085905c9936db21c7233581d9c58565434
tings0802 <nick19980922@gmail.com>
2020-07-03 20:36:12 +0800

    modify: run script only when it is called

--- scripts/intel.sh
+++ scripts/intel.sh
@@ -3,17 +3,19 @@
 # Date: 2020-07-02 Thu.
 # Desc: create modulefiles for Intel C Compiler, Intel MKL, and Intel MPI
 
-INSTALL_DIR=/root/sources/env2-code
-git clone https://git.code.sf.net/p/env2/code ${INSTALL_DIR}
+function intel_modules() {
+	INSTALL_DIR=/root/sources/env2-code
+	git clone https://git.code.sf.net/p/env2/code ${INSTALL_DIR}
 
-echo "#%Module" > /usr/share/modules/modulefiles/icc
-perl ${INSTALL_DIR}/env2 -from bash -to modulecmd \
-"/opt/intel/compilers_and_libraries_2020.0.166/linux/bin/compilervars.sh intel64" >> /usr/share/modules/modulefiles/icc
+	echo "#%Module" > /usr/share/modules/modulefiles/icc
+	perl ${INSTALL_DIR}/env2 -from bash -to modulecmd \
+	"/opt/intel/compilers_and_libraries_2020.0.166/linux/bin/compilervars.sh intel64" >> /usr/share/modules/modulefiles/icc
 
-echo "#%Module" > /usr/share/modules/modulefiles/mkl
-perl ${INSTALL_DIR}/env2 -from bash -to modulecmd \
-"/opt/intel/compilers_and_libraries_2020.0.166/linux/mkl/bin/mklvars.sh intel64" >> /usr/share/modules/modulefiles/mkl
+	echo "#%Module" > /usr/share/modules/modulefiles/mkl
+	perl ${INSTALL_DIR}/env2 -from bash -to modulecmd \
+	"/opt/intel/compilers_and_libraries_2020.0.166/linux/mkl/bin/mklvars.sh intel64" >> /usr/share/modules/modulefiles/mkl
 
-echo "#%Module" > /usr/share/modules/modulefiles/impi
-perl ${INSTALL_DIR}/env2 -from bash -to modulecmd \
-"/opt/intel/compilers_and_libraries_2020.0.166/linux/mpi/intel64/bin/mpivars.sh" >> /usr/share/modules/modulefiles/impi
+	echo "#%Module" > /usr/share/modules/modulefiles/impi
+	perl ${INSTALL_DIR}/env2 -from bash -to modulecmd \
+	"/opt/intel/compilers_and_libraries_2020.0.166/linux/mpi/intel64/bin/mpivars.sh" >> /usr/share/modules/modulefiles/impi
+}
